a2b0200bc0e96e9f4025c8bd4fdfdb06b7c7556c
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2015-12-23 13:39:22 +0500

    Оказывается оно не работало

--- parser.py
+++ parser.py
@@ -8,16 +8,15 @@ root = tree.getroot()
 artist = name = 0
 
 for track in root.findall("./dict/dict/dict/"):
-	for node in track:
-		if node.tag == "key" and node.text == "Name":
-			name = 1
-		if node.tag == "string" and name == 1:
-			nametext = node.text
-			name = 0
-		if node.tag == "key" and node.text == "Artist":
-			artist = 1
-		if node.tag == "string" and artist == 1:
-			line = u'%s - %s' % (node.text, nametext)
-			print line.encode('utf-8')
-			artist = 0
+	if track.tag == "key" and track.text == "Name":
+		name = 1
+	if track.tag == "string" and name == 1:
+		nametext = track.text
+		name = 0
+	if track.tag == "key" and track.text == "Artist":
+		artist = 1
+	if track.tag == "string" and artist == 1:
+		line = u'%s - %s' % (track.text, nametext)
+		print line.encode('utf-8')
+		artist = 0
 
