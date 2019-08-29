8550edf5949fa55db0eedea9ed5f694b6d632a25
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2019-08-29 22:16:43 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 python parser.py > media.tmp
 sort -u media.tmp > plaintext_library.txt
 rm -f media.tmp
-bash statistik.sh plaintext_library.txt | tee report.txt
+bash statistic.sh plaintext_library.txt | tee report.txt
